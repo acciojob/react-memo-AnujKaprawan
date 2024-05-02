cd24db0e89bc9d65b03ffab5adc6e0fc1bab7f01
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-05-02 15:37:16 +0000

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,75 +1,72 @@
-import React, { useState, useMemo } from "react";
-import './../styles/App.css';
 
-// Functional component for displaying skills
-const SkillList = React.memo(({ skills }) => {
-  return (
-    <div>
-     
-      <ul id="item-jumbotron">
-        {skills.map((skill, index) => (
-          <li key={index} id={`skill-&{index}`}>   {skill}</li>
-        ))}
-      </ul>
-    </div>
-  );
-});
+
+
+import React, { useState } from 'react';
+import UseMemo from './UseMemo';
+import ReactMemo from './ReactMemo';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState([]);
-  const [skills, setSkills] = useState([]);
-  
-  // Expensive calculation
-  const calculation = useMemo(() => expensiveCalculation(count), [count]);
+  const [tasks, setTasks] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [customTask, setCustomTask] = useState('');
 
-  const increment = () => {
-    setCount((c) => c + 1);
+  const handleAddTodo = () => {
+    setTasks([...tasks, 'New todo']);
   };
 
-  const addTodo = () => {
-    setTodos((t) => [...t, "New Todo"]);
+  const handleIncrement = () => {
+    setCounter(counter + 1);
   };
 
-  const addSkill = (newSkill) => {
-    setSkills((prevSkills) => [...prevSkills, newSkill]);
+  const handleCustomTaskChange = (e) => {
+    setCustomTask(e.target.value);
+  };
+
+  const handleCustomTaskSubmit = () => {
+    if (customTask.length > 5) {
+      setTasks([...tasks, customTask]);
+      setCustomTask('');
+    } else {
+      alert('Task must be more than 5 characters long.');
+    }
+  };
+
+  const handleRemoveTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
   };
 
   return (
     <div id="main">
-      <div>
-        <h1>React.useMemo</h1>
-        <h2>My Todos</h2>
-        {todos.map((todo, index=0) => {
-          return <p id ="todo-${index++}" key={index}>{todo}</p>;
-        })}
-        <button id="add-todo-btn" onClick={addTodo}>Add Todo</button>
+      <button id="add-todo-btn" onClick={handleAddTodo}>Add todo</button>
+      
+      <button id="increment-btn" onClick={handleIncrement}>{counter}</button>
+      <input
+        id="skill-input"
+        type="text"
+        value={customTask}
+        onChange={handleCustomTaskChange}
+        placeholder="Enter custom task"
+      />
+      <button onClick={handleCustomTaskSubmit} id="skill-btn">Add Skill</button>
+      <div id="item-jumbotron">
+      {tasks.map((task, index) => (
+    <div key={index}>{task}</div>
+  ))}
       </div>
-      <hr />
-      <div>
-        Count: {count}
-        <button id="incr-cnt" onClick={increment}>+</button>
-        <h2 >Expansive Calculation</h2>
-        {calculation}
-      </div>
-      <hr />
-      <div>
-        <h2>React.Memo</h2>
-        <input type="text" id="skill-input" placeholder="Enter skill" />
-        <button id="skill-btn" onClick={() => addSkill(document.getElementById("skill-input").value)}>Add Skill</button>
-      </div>
-      <SkillList skills={skills} />
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button id={`todo-${index}`} onClick={() => handleRemoveTask(index)}>New Todo</button>
+          </li>
+        ))}
+      </ul>
+      <UseMemo />
+      <ReactMemo />
     </div>
   );
-};
-
-// Expensive calculation function
-const expensiveCalculation = (num) => {
-  
-  for (let i = 0; i < 1000000000; i++) {
-    num += 1;
-  }
-  return num;
 };
 
 export default App;
